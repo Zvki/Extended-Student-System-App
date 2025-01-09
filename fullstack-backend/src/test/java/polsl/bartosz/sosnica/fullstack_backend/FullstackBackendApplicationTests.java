297f9b4092e892abd8d3e279fa1fe6a1c61003a6
Bartosz Sośnica <115103008+Zvki@users.noreply.github.com>
2025-01-09 14:54:18 +0100

    feat: added basic project and connected it with supabase database

---
+++ fullstack-backend/src/test/java/polsl/bartosz/sosnica/fullstack_backend/FullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package polsl.bartosz.sosnica.fullstack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
