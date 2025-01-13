5a1c683e046e557ded6aa509f8fb04a0f367b5d7
Bartosz Sośnica <115103008+Zvki@users.noreply.github.com>
2025-01-13 22:21:02 +0100

    feat: added lombok to project and slightly changed user entity

--- fullstack-backend/src/main/java/polsl/bartosz/sosnica/fullstack_backend/model/UserModel.java
+++ fullstack-backend/src/main/java/polsl/bartosz/sosnica/fullstack_backend/model/UserModel.java
@@ -1,50 +1,43 @@
 package polsl.bartosz.sosnica.fullstack_backend.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "Users")
+@Getter
+@Setter
+@AllArgsConstructor
 public class UserModel {
 
     @Id
     @GeneratedValue
     private Long id;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "email")
     private String email;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
