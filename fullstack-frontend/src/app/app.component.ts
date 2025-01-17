5692bfa2d13187fc288f639d58c2567603292d9d
Bartosz Sośnica <bartoszsosnica@gmail.com>
2025-01-17 16:57:19 +0100

    feat: add login page component and navbar, update routing and styles for improved UI

--- fullstack-frontend/src/app/app.component.ts
+++ fullstack-frontend/src/app/app.component.ts
@@ -1,31 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 
 })
 export class AppComponent {
-  title = 'fullstack-frontend';
-
-  handleClick() {
-    const apiUrl = "http://localhost:8080";
-    if (!apiUrl) {
-      throw new Error('API_URL is not defined');
-    }
-    fetch(apiUrl + "/getnull")
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        // handle the data from the backend here
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }
 }
---
+++ fullstack-frontend/src/app/login-page/login-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css'
+})
+export class LoginPageComponent {
+
+}
