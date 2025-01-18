90ec48201f1da06fa73abacd1582a1eda8dd35c4
Bartosz Sośnica <bartoszsosnica@gmail.com>
2025-01-18 11:46:06 +0100

    feat: add landing page and footer components, update app routing and navbar for improved navigation

--- fullstack-frontend/src/app/app.component.ts
+++ fullstack-frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, NavbarComponent],
+  imports: [RouterOutlet, RouterLink, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 
--- fullstack-frontend/src/app/app.routes.ts
+++ fullstack-frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
+{ path: '', component: LandingPageComponent },
 { path: 'login', component: LoginPageComponent }
 ];
