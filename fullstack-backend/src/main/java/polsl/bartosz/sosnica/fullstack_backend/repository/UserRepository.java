ead98707c11a247dbf71d201784cc8c4ebd1e412
Zvki <bartoszsosnica@gmail.com>
2025-02-26 13:55:48 +0100

    refactor: remove unused AuthRepository and UserRepository; update landing hero component to use buttons for quick actions

--- fullstack-backend/src/main/java/polsl/bartosz/sosnica/fullstack_backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package polsl.bartosz.sosnica.fullstack_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import polsl.bartosz.sosnica.fullstack_backend.model.UserModel;
-
-public interface UserRepository extends JpaRepository<UserModel, Long> {
-}
