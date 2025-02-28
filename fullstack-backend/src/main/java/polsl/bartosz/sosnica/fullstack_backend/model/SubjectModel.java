d82abe06f3fba747f3e912c9073de141da62460a
Zvki <bartoszsosnica@gmail.com>
2025-02-28 21:30:35 +0100

    feat: add description field to SubjectModel for enhanced subject details

--- fullstack-backend/src/main/java/polsl/bartosz/sosnica/fullstack_backend/model/SubjectModel.java
+++ fullstack-backend/src/main/java/polsl/bartosz/sosnica/fullstack_backend/model/SubjectModel.java
@@ -1,15 +1,10 @@
 package polsl.bartosz.sosnica.fullstack_backend.model;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -26,5 +21,8 @@ public class SubjectModel {
 
     @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
+    private String description;
 
 }
