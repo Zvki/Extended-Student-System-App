1131ea51f0c5a382c97971c10c651ce0252d032b
Bartosz Sośnica <115103008+Zvki@users.noreply.github.com>
2025-03-09 19:16:24 +0100

    docs: add Javadoc comments to SubjectController for improved clarity and documentation

--- fullstack-backend/src/main/java/polsl/bartosz/sosnica/fullstack_backend/controller/SubjectController.java
+++ fullstack-backend/src/main/java/polsl/bartosz/sosnica/fullstack_backend/controller/SubjectController.java
@@ -13,16 +13,44 @@ import polsl.bartosz.sosnica.fullstack_backend.model.SubjectModel;
 import polsl.bartosz.sosnica.fullstack_backend.response.ApiResponse;
 import polsl.bartosz.sosnica.fullstack_backend.service.SubjectService;
 
+/**
+ * Controller responsible for handling subject-related operations.
+ * 
+ * <p>
+ * This controller provides endpoints for retrieving subjects that are not yet
+ * enrolled by a specific user.
+ * </p>
+ * 
+ * @author Bartosz Sosnica
+ */
 @RestController
 public class SubjectController {
 
     private ISubjectService subjectService;
 
+    /**
+     * Constructs a SubjectController with the necessary service dependency.
+     * 
+     * @param subjectService the service managing subject-related operations
+     */
     @Autowired
     public SubjectController(SubjectService subjectService) {
         this.subjectService = subjectService;
     }
 
+    /**
+     * Retrieves a list of subjects that a given user is not enrolled in.
+     * 
+     * <p>
+     * This method fetches all subjects available for a user to enroll in, excluding
+     * those the user is already enrolled in.
+     * </p>
+     * 
+     * @param userId the ID of the user for whom available subjects are being
+     *               retrieved
+     * @return a {@code ResponseEntity} containing a list of available subjects or
+     *         an error message
+     */
     @GetMapping("/usersavailablesubjects/{userId}")
     public ResponseEntity<?> findSubjectNotEnrolledByUser(@PathVariable Long userId) {
 
