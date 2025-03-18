d24b5792dc8235bada5c7a86158fcd602fb13403
Bartosz Sośnica <115103008+Zvki@users.noreply.github.com>
2025-03-18 14:08:24 +0100

    feat: added user association with subject

--- fullstack-backend/src/main/java/polsl/bartosz/sosnica/fullstack_backend/model/SubjectModel.java
+++ fullstack-backend/src/main/java/polsl/bartosz/sosnica/fullstack_backend/model/SubjectModel.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -24,5 +26,9 @@ public class SubjectModel {
 
     @Column(name = "description")
     private String description;
+
+    @ManyToOne()
+    @JoinColumn(name = "user_id")
+    private UserModel user;
 
 }
