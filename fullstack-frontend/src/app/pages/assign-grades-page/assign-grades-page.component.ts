a2da4fccf086678da9fff21be78ed9ab9f4c9cb5
Bartosz Sośnica <115103008+Zvki@users.noreply.github.com>
2025-03-27 23:22:10 +0100

    feat: replace ChooseSubjectComponent with AssignGradesComponent in AssignGradesPage

--- fullstack-frontend/src/app/pages/assign-grades-page/assign-grades-page.component.ts
+++ fullstack-frontend/src/app/pages/assign-grades-page/assign-grades-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { ChooseSubjectComponent } from '../../components/choose-subject/choose-subject.component';
+import { AssignGradesComponent } from '../../components/assign-grades/assign-grades.component';
 
 @Component({
   selector: 'app-assign-grades-page',
   standalone: true,
-  imports: [FooterComponent, NavbarComponent, ChooseSubjectComponent],
+  imports: [FooterComponent, NavbarComponent, ChooseSubjectComponent, AssignGradesComponent],
   templateUrl: './assign-grades-page.component.html',
   styleUrl: './assign-grades-page.component.css'
 })
